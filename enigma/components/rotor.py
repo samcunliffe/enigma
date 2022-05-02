60069f5f37e7fe911b26f3bbd9383af82cdee323
Sam Cunliffe <sam.cunliffe@gmail.com>
2022-05-02 16:25:25 +0100

    Forward and backward currents through rotors.

--- enigma/components/rotor.py
+++ enigma/components/rotor.py
@@ -1,3 +1,4 @@
+import string
 from .utils import to_position, verify_alphabet
 
 
@@ -14,25 +15,44 @@ class Rotor:
         self.n = to_position(notch)
         self.name = name
         self.start = 0
+        self._get_inverse_map()
 
     def __str__(self):
         return self.name if self.name else self.a
 
-    def __call__(self, c):
+    def _get_inverse_map(self):
+        """Given the forward wiring in the "A" position, we need to know the
+        inverse map (also in the "A" position)."""
+
+        # pair up the map with the alphabet ABC..Z
+        list_of_tuples = [(a, b) for a, b in zip(self.a, string.ascii_uppercase)]
+        # now reorder based on the alphebetised map (which scrambles ABC...)
+        self.b = "".join([second for _, second in sorted(list_of_tuples)])
+
+    def forward(self, c):
+        """Current through the rotor in the forward direction"""
         point = self.start + to_position(c)
         return (self.a[point:] + self.a[:point])[0]
 
+    def backward(self, c):
+        """Current going backwards through the rotor"""
+        point = self.start + to_position(c)
+        return (self.b[point:] + self.b[:point])[0]
+
     def rotate(self):
+        """Rotate the rotor"""
         if self.start < 26:
             self.start += 1
         else:
             self.start = 0
 
     def at_notch(self):
+        """Are we at a notch position?
+        (the machine may need to rotate other rotors)"""
         return self.start == self.n
 
     def reset(self):
         self.start = 0
 
-    def setup(self, starting_position):
-        raise NotImplementedError()
+    #def setup(self, starting_position):
+        #raise NotImplementedError()
--- tests/test_rotors.py
+++ tests/test_rotors.py
@@ -10,15 +10,25 @@ def test_invalid_construction():
         r = components.Rotor("AABCDEFGHIJKLMNOPQRSTUVWXYZ", "A")
 
 
-def test_single_rotor():
-    """First call to rotator R1"""
-    assert components.r1("A") == "E"
+def test_single_rotor_forward():
+    """Forward call to rotator I"""
+    assert components.r1.forward("A") == "E"
+    assert components.r1.forward("B") == "K"
+    assert components.r1.forward("C") == "M"
+    assert components.r1.forward("D") == "F"
+
+def test_single_rotor_backward():
+    """Backward call to rotator I"""
+    assert components.r1.backward("E") == "A"
+    assert components.r1.backward("K") == "B"
+    assert components.r1.backward("M") == "C"
+    assert components.r1.backward("F") == "D"
 
 
 def test_repeated_rotation():
     """Repeated rotations should wrap around"""
     [components.r1.rotate() for i in range(26)]
-    assert components.r1("A") == "E"
+    assert components.r1.forward("A") == "E"
 
 
 def test_notch_position():
