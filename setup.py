3f52c515080e7b326acd95ce890a7c9394df3362
Sam Cunliffe <sam.cunliffe@gmail.com>
2022-05-04 12:45:45 +0100

    Package description in setup.py

--- setup.py
+++ setup.py
@@ -3,4 +3,12 @@ Python package setup script for samcunliffe/enigma.
 As we're pure python, there's not much to do.
 """
 from setuptools import setup
-setup()
+setup(
+    name='enigma',
+    version='1.0.1',
+    description='Sams enigma machine',
+    url='https://github.com/samcunliffe/enigma',
+    author='Sam Cunliffe',
+    license='MIT 2-clause',
+    packages=['enigma'],
+)
